df4ea6b5781b78239cf5711d13d7ff86358b4256
hoabao0809 <hoabao.0809@gmail.com>
2022-10-25 11:30:34 +0700

    check authorization before adminDashboard

--- src/app/pages/admin-template/dashboard/dashboard.component.ts
+++ src/app/pages/admin-template/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { DataService } from 'src/app/_core/services/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,15 +14,29 @@ export class DashboardComponent implements OnInit {
 
   userEdit: any;
 
-  constructor(private router: Router) {}
+  accessDashboard = new Subscription();
 
-  ngOnInit(): void {}
+  constructor(private router: Router, private data: DataService) {}
+
+  ngOnInit(): void {
+    this.getDashboardAccess();
+  }
+
+  getDashboardAccess() {
+    this.accessDashboard = this.data.get('admin').subscribe((res) => {
+      console.log(res);
+    });
+  }
 
   openModalUser(user: any) {
     this.userEdit = user;
   }
 
   openModalJob(event: any) {
-    this.itemJob = event
+    this.itemJob = event;
+  }
+
+  ngOnDestroy() {
+    this.accessDashboard.unsubscribe();
   }
 }
